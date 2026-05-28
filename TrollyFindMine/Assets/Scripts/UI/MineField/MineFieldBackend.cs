eeb361c4e19f9c510cfc604b2aa737f192f7a148
StuntJ <dlwornr0919@gmail.com>
2026-05-28 12:39:37 +0900

    feat : mineFieldBackend에 몇가지 기능 더 추가

--- TrollyFindMine/Assets/Scripts/UI/MineField/MineFieldBackend.cs
+++ TrollyFindMine/Assets/Scripts/UI/MineField/MineFieldBackend.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 
 
 public class MineFieldBackend : MonoBehaviour
 {
+    #region  event
+    public event Action<Coord> OnSelectedCoordChanged; // TODO : Frontend에서 Highlight 변경시키는 함수 구독시킬것
+    #endregion
+    
     #region Field
 
     [SerializeField] private ButtonFrontend buttonController;
@@ -14,18 +20,21 @@ public class MineFieldBackend : MonoBehaviour
     private int row;
     private int col;
     private int leftMineNumber;
+    private int totalMineNumber;
     private int openedCellNumber = 0;
     private bool mapGeneratedFlag = false;
     private static readonly int[] dx = { 0, 0, -1, 1, 1, 1, -1, -1 };
     private static readonly int[] dy = { 1, -1, 0, 0, 1, -1, 1, -1 };
-
+    private Coord selectedCoord; //현재 선택중인 cell
+    
     #endregion
     
     #region Property
 
     public int LeftMineNumber => leftMineNumber;
     public int OpenedCellNumber => openedCellNumber;
-
+    public Coord SelectedCoord => selectedCoord;
+    
     #endregion
     
     #region Private Methods
@@ -35,8 +44,9 @@ public class MineFieldBackend : MonoBehaviour
         return (0 <= coord.x && coord.x < row && 0 <= coord.y && coord.y < col);
     }
     
-    private void GenerateMap(Coord startPoint)
+    private void GenerateMap()
     {
+        Coord startPoint = selectedCoord;
         grid[startPoint.x, startPoint.y] = 0;
         for (int i = 0; i < 8; i++)
         {
@@ -49,8 +59,8 @@ public class MineFieldBackend : MonoBehaviour
         int toGenerateMine = leftMineNumber;
         while (toGenerateMine > 0) //지뢰 배열하기
         {
-            int nx = Random.Range(0, row);
-            int ny = Random.Range(0, col);
+            int nx = UnityEngine.Random.Range(0, row);
+            int ny = UnityEngine.Random.Range(0, col);
             if (grid[nx, ny] != -2) continue;
             else
             {
@@ -86,6 +96,13 @@ public class MineFieldBackend : MonoBehaviour
         
         //TODO : Frontend에 변경사항 적용하기
     }
+
+    private void OpenCellSequence(Coord coord)
+    {
+        openedCells[coord.x, coord.y] = true;
+        openedCellNumber++;
+        //TODO : Frontend 업데이트
+    }
     
     #endregion
     
@@ -105,11 +122,15 @@ public class MineFieldBackend : MonoBehaviour
         
         //TODO: buttonFrontend에서 N,M초기화
         //TODO: GridFrontend에서 N,M초기화
+        selectedCoord = new Coord(UnityEngine.Random.Range(0, row), UnityEngine.Random.Range(0, col)); //selectedCoord 초기화
+        OnSelectedCoordChanged.Invoke(selectedCoord); //highlight
         leftMineNumber = numOfMine;
+        totalMineNumber = numOfMine;
     }
 
-    public void OpenCell(Coord coord)
+    public void OpenCellWithLeftClick()
     {
+        Coord coord = selectedCoord;
         if (mapGeneratedFlag)
         {
             if (grid[coord.x, coord.y] == -2) //지뢰를 클릭했을때
@@ -121,22 +142,81 @@ public class MineFieldBackend : MonoBehaviour
             {
                 if (grid[coord.x, coord.y] == 0) //bfs 필요
                 {
-                    //TODO : BFS로 cell open
+                    //BFS로 cell open
+                    Queue<Coord> queue = new Queue<Coord>();
+                    bool[,] chk = new bool[row, col];
+                    queue.Enqueue(coord);
+                    chk[coord.x, coord.y] = true;
+                    while (queue.Count != 0)
+                    {
+                        Coord here = queue.Dequeue();
+
+                        if (grid[here.x, here.y] == 0)
+                        {
+                            for (int i = 0; i < 8; i++)
+                            {
+                                int nx = here.x + dx[i];
+                                int ny = here.y + dy[i];
+                                Coord nc = new Coord(nx, ny);
+                                if (IsInRange(nc) && grid[nc.x,nc.y] != -1)
+                                {
+                                    chk[nc.x, nc.y] = true;
+                                    if (openedCells[nc.x, nc.y] == false)
+                                    {
+                                        OpenCellSequence(nc);    
+                                    }
+                                    if(grid[nc.x,nc.y]==0) queue.Enqueue(nc);
+                                }
+                            }    
+                        }
+                    }
                 }
                 else //그 숫자 하나만 열면 됨
                 {
-                    //TODO : cell 하나 open
+                    OpenCellSequence(coord);
+                }
+                //게임 승리 조건 확인 
+                if (row * col - openedCellNumber == totalMineNumber)
+                {
+                    //TODO : 게임 승리 로직
                 }
             }
             
-            //TODO : Frontend Update
+            
         }
         else //아직 맵이 생성되지 않았을때 -> map을 generate해야함 
         {
-            GenerateMap(coord);
+            GenerateMap();
             mapGeneratedFlag = true;
-            OpenCell(coord);
+            OpenCellWithLeftClick();
         }
+    }
+
+    public void FlagCellWithRightClick()
+    {
+        Coord coord = selectedCoord;
+        if (openedCells[coord.x, coord.y] == false)
+        {
+            //TODO : frontend에서 깃발 꽂는 로직
+        } 
+    }
+
+    #endregion
+
+    #region 입력 시스템과 연결
+
+    public void SelectedCellMove(Vector2 dir) //dir은 normalize된 Vector2;
+    {
+        // 배열좌표계로 Vector2 변환
+        int nx = selectedCoord.x - (int)dir.y;
+        int ny = selectedCoord.y + (int)dir.x;
+        Coord nc = new Coord(nx, ny);
+        if (IsInRange(nc))
+        {
+            selectedCoord = nc;
+            OnSelectedCoordChanged.Invoke(selectedCoord);
+        }
+        
     }
 
     #endregion
